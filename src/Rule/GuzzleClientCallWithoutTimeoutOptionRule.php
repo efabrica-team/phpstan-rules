ed2ceb31a806da60169034032e01692bc91b978e
Michal Lulco <lulco@efabrica.sk>
2021-09-28 01:12:41 +0200

    Rewritten rule GuzzleClientCallWithoutTimeoutOptionRule to get option values from properties and method calls

--- src/Rule/GuzzleClientCallWithoutTimeoutOptionRule.php
+++ src/Rule/GuzzleClientCallWithoutTimeoutOptionRule.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace Efabrica\PHPStanRules\Rule;
 
+use PhpParser\ConstExprEvaluator;
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\PropertyProperty;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\NodeFinder;
 use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
 use PHPStan\Type\ObjectType;
 use Symplify\Astral\NodeValue\NodeValueResolver;
 
 final class GuzzleClientCallWithoutTimeoutOptionRule implements Rule
 {
+    private NodeFinder $nodeFinder;
+
     private NodeValueResolver $nodeValueResolver;
 
     /**
@@ -37,51 +49,114 @@ final class GuzzleClientCallWithoutTimeoutOptionRule implements Rule
         'deleteAsync' => 1,
     ];
 
-    public function __construct(NodeValueResolver $nodeValueResolver)
+    private ConstExprEvaluator $constExprEvaluator;
+
+    public function __construct(NodeFinder $nodeFinder, NodeValueResolver $nodeValueResolver)
     {
+        $this->nodeFinder = $nodeFinder;
         $this->nodeValueResolver = $nodeValueResolver;
     }
 
     public function getNodeType(): string
     {
-        return MethodCall::class;
+        return InClassNode::class;
     }
 
     /**
-     * @param MethodCall $node
+     * @param InClassNode $node
      */
-    public function processNode(Node $node, Scope $scope): array
+    public function processNode(Node $inClassNode, Scope $scope): array
     {
-        $callerType = $scope->getType($node->var);
-        if (!$callerType instanceof ObjectType || !$callerType->isInstanceOf('GuzzleHttp\Client')->yes()) {
-            return [];
-        }
+        $file = $scope->getFile();
+        $methodCallNodes = $this->nodeFinder->findInstanceOf([$inClassNode->getOriginalNode()], MethodCall::class);
+        $this->constExprEvaluator = new ConstExprEvaluator(fn (Expr $expr) => $this->resolveByNode($inClassNode, $scope, $expr));
 
-        $methodName = $node->name->name;
-        if (!isset($this->methodOptionArgPosition[$methodName])) {
-            return [];
-        }
-
-        $argPosition = $this->methodOptionArgPosition[$methodName];
-        $argAtPosition = $node->args[$argPosition] ?? null;
-        if ($argAtPosition === null) {
-            return [
-                'Method GuzzleHttp\Client::' . $methodName . ' is called without timeout optionx',
-            ];
-        }
-
-        $options = $this->nodeValueResolver->resolveWithScope($argAtPosition->value, $scope);
-        if (is_array($options)) {
-            if (!isset($options['timeout'])) {
-                return [
-                    'Method GuzzleHttp\Client::' . $methodName . ' is called without timeout option',
-                ];
+        $errors = [];
+        foreach ($methodCallNodes as $node) {
+            $callerType = $scope->getType($node->var);
+            if (!$callerType instanceof ObjectType || !$callerType->isInstanceOf('GuzzleHttp\Client')->yes()) {
+                continue;
             }
-            return [];
+
+            $methodName = $node->name->name;
+            if (!isset($this->methodOptionArgPosition[$methodName])) {
+                continue;
+            }
+
+            $argPosition = $this->methodOptionArgPosition[$methodName];
+            $argAtPosition = $node->args[$argPosition] ?? null;
+            if ($argAtPosition === null) {
+                $errors[] = RuleErrorBuilder::message('Method GuzzleHttp\Client::' . $methodName . ' is called without timeout option')->file($file)->line($node->getStartLine())->build();
+            }
+
+            $options = $this->nodeValueResolver->resolveWithScope($argAtPosition->value, $scope);
+            if (!is_array($options)) {
+                $options = $this->constExprEvaluator->evaluateDirectly($argAtPosition->value);
+            }
+
+            if (is_array($options)) {
+                if (!array_key_exists('timeout', $options)) {
+                    $errors[] = RuleErrorBuilder::message('Method GuzzleHttp\Client::' . $methodName . ' is called without timeout option')->file($file)->line($node->getStartLine())->build();
+                }
+            }
         }
 
-        return [
-            'Method GuzzleHttp\Client::' . $methodName . ' is probably called without timeout option',
-        ];
+        return $errors;
+    }
+
+    private function resolveByNode(InClassNode $inClassNode, Scope $scope, Expr $expr)
+    {
+        if ($expr instanceof PropertyFetch) {
+            /** @var PropertyProperty[] $propertyNodes */
+            $propertyNodes = $this->nodeFinder->findInstanceOf([$inClassNode->getOriginalNode()], PropertyProperty::class);
+            $properties = [];
+            foreach ($propertyNodes as $propertyNode) {
+                $properties[$propertyNode->name->name] = $propertyNode->default ? $this->nodeValueResolver->resolveWithScope($propertyNode->default, $scope) : null;
+            }
+
+            if (is_string($expr->name)) {
+                $propertyFetchName = $expr->name;
+            } elseif ($expr->name instanceof Identifier) {
+                $propertyFetchName = $expr->name->name;
+            } else {
+                $propertyFetchName = $this->nodeValueResolver->resolveWithScope($expr->name, $scope);
+            }
+
+            if ($propertyFetchName === null) {
+                return null;
+            }
+
+            return $properties[$propertyFetchName] ?? null;
+        }
+
+        if ($expr instanceof MethodCall) {
+            $methodName = $expr->name instanceof Identifier ? $expr->name->name : $this->nodeValueResolver->resolveWithScope($expr->name, $scope);
+            if (!$methodName) {
+                return null;
+            }
+            /** @var ClassMethod[] $classMethodNodes */
+            $classMethodNodes = $this->nodeFinder->findInstanceOf([$inClassNode->getOriginalNode()], ClassMethod::class);
+            foreach ($classMethodNodes as $classMethodNode) {
+                if ($classMethodNode->name->name !== $methodName) {
+                    continue;
+                }
+                /** @var Return_|null $returnStatement */
+                $returnStatement = $this->nodeFinder->findFirstInstanceOf($classMethodNode, Return_::class);
+                if (!$returnStatement) {
+                    return null;
+                }
+                if ($returnStatement->expr === null) {
+                    return null;
+                }
+
+                $value = $this->nodeValueResolver->resolveWithScope($returnStatement->expr, $scope);
+                if ($value === null) {
+                    $value = $this->constExprEvaluator->evaluateDirectly($returnStatement->expr);
+                }
+                return $value;
+            }
+        }
+
+        return null;
     }
 }
