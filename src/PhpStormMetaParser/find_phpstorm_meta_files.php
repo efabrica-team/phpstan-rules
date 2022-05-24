0f75d664080c00e412c80d318c0aa322bf80cad5
Michal Lulco <lulco@efabrica.sk>
2022-05-24 23:33:20 +0200

    Sniffer fix

--- src/PhpStormMetaParser/find_phpstorm_meta_files.php
+++ src/PhpStormMetaParser/find_phpstorm_meta_files.php
@@ -7,5 +7,9 @@ declare(strict_types=1);
 $rootDir = (string)getcwd();
 
 // credits: https://stackoverflow.com/questions/22761554/how-to-get-all-class-names-inside-a-particular-namespace
-$allFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootDir, FilesystemIterator::KEY_AS_PATHNAME|FilesystemIterator::CURRENT_AS_FILEINFO|FilesystemIterator::SKIP_DOTS));
+$allFiles = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator(
+        $rootDir,
+        FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS
+    ));
 return new RegexIterator($allFiles, '/\.phpstorm.meta.php/');
