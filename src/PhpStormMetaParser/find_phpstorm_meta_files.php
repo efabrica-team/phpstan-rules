b6f8aa47ab9aec7284fc648f155ed42ba460603e
Michal Lulco <lulco@efabrica.sk>
2022-05-24 23:31:30 +0200

    GitHub actions + fixes

--- src/PhpStormMetaParser/find_phpstorm_meta_files.php
+++ src/PhpStormMetaParser/find_phpstorm_meta_files.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 // TODO pass somehow dirs to include if don't want to parse all
 
-$rootDir = getcwd();
+$rootDir = (string)getcwd();
 
 // credits: https://stackoverflow.com/questions/22761554/how-to-get-all-class-names-inside-a-particular-namespace
 $allFiles = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($rootDir, FilesystemIterator::KEY_AS_PATHNAME|FilesystemIterator::CURRENT_AS_FILEINFO|FilesystemIterator::SKIP_DOTS));
