077ebb6e5c65489e80be82b0742375c043124db5
Richard Popelis <popelis@efabrica.sk>
2023-07-25 14:29:01 +0200

    EnforceArrowFunctionRule

---
+++ src/Rule/General/EnforceArrowFunctionRule.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Efabrica\PHPStanRules\Rule\General;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Closure;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+
+class EnforceArrowFunctionRule
+{
+    public function getNodeType(): string
+    {
+        return Closure::class;
+    }
+
+    /**
+     * @param Closure $node
+     * @param Scope   $scope
+     * @return array|string[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if ($node->stmts === null || count($node->stmts) !== 1) {
+            return [];
+        }
+
+        $onlyStatement = $node->stmts[0];
+        if (!$onlyStatement instanceof Node\Stmt\Return_) {
+            return [];
+        }
+
+        return [
+            'Closure only has a single return statement. Use an arrow function instead.',
+        ];
+    }
+}
